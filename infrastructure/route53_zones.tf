2c88ae14a0ff2c8eecc2345daee7b251a6e35594
Marc Bachmann <marc.brookman@gmail.com>
2018-07-09 16:34:42 +0200

    Move services to separate domain
    Elasticsearch-HQ doesn't work properly on a subpath and
    Kibana is also easier to open when you can type
    `kibana` in the browser omnibar 😁

--- infrastructure/route53_zones.tf
+++ infrastructure/route53_zones.tf
@@ -1,10 +1,13 @@
 variable "aws_production_access_key" {}
 variable "aws_production_secret_key" {}
 variable "aws_production_region" {}
+
 variable "cluster_domain_zone" {}
-variable "cluster_domain_monitoring" {}
 variable "cluster_domain_rancher" {}
+variable "cluster_domain_monitoring" {}
 variable "cluster_domain_prometheus" {}
+variable "cluster_domain_kibana" {}
+variable "cluster_domain_elasticsearch_hq" {}
 
 provider "aws" {
   version = ">= 1.15"
@@ -66,30 +69,20 @@ resource "aws_route53_record" "prometheus" {
   records = ["${digitalocean_droplet.monitoring.*.ipv4_address}"]
 }
 
+resource "aws_route53_record" "elasticsearch-hq" {
+  zone_id = "${data.aws_route53_zone.main.id}"
+  name    = "${var.cluster_domain_elasticsearch_hq}"
+  type    = "A"
+  ttl     = "30"
 
-# resource "aws_route53_record" "logs" {
-#   zone_id = "${aws_route53_zone.workspace.zone_id}"
-#   name    = "logs.${terraform.workspace}.livingdocs.io"
-#   type    = "A"
-#   ttl     = "30"
+  records = ["${digitalocean_droplet.monitoring.*.ipv4_address}"]
+}
 
-#   records = [
-#     "${aws_route53_zone.workspace.name_servers.0}",
-#     "${aws_route53_zone.workspace.name_servers.1}",
-#     "${aws_route53_zone.workspace.name_servers.2}",
-#     "${aws_route53_zone.workspace.name_servers.3}",
-#   ]
-# }
+resource "aws_route53_record" "kibana" {
+  zone_id = "${data.aws_route53_zone.main.id}"
+  name    = "${var.cluster_domain_kibana}"
+  type    = "A"
+  ttl     = "30"
 
-# resource "aws_route53_health_check" "logs" {
-#   fqdn              = "logs.${terraform.workspace}.livingdocs.io"
-#   port              = 80
-#   type              = "HTTP"
-#   resource_path     = "/"
-#   failure_threshold = "5"
-#   request_interval  = "10"
-
-#   tags = {
-#     Name = "logs-server-${terraform.workspace}-health-check"
-#   }
-# }
+  records = ["${digitalocean_droplet.monitoring.*.ipv4_address}"]
+}
